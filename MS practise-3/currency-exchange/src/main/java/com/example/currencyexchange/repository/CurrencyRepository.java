b5dea8d524b4048e9c0aa5fa837d374919773fa7
KhairulIslamAzam <khairulislam.azam@gmail.com>
2021-03-09 14:09:44 +0600

    delete mservice

--- MS practise-3/currency-exchange/src/main/java/com/example/currencyexchange/repository/CurrencyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.currencyexchange.repository;
-
-import com.example.currencyexchange.model.CurrencyExchange;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CurrencyRepository extends JpaRepository<CurrencyExchange, Long> {
-
-    CurrencyExchange findByCurrencyFromAndCurrencyTo(String from, String to);
-}
